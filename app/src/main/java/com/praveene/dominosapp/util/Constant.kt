3d56d9b6f122a4bb717edc073c7514618c86f37a
PraveenAade15 <laxmipraveen101@gmail.com>
2021-09-19 12:16:41 +0530

    Fragment add and repository done

---
+++ app/src/main/java/com/praveene/dominosapp/util/Constant.kt
@@ -0,0 +1,11 @@
+package com.praveene.dominosapp.util
+
+object Constants {
+
+    const val RESPONSE_MODEL_TABLE = "response_model_table"
+    //const val BASE_URL = "https://api.jsonbin.io/"
+    const val BASE_URL = "https://android.free.beeceptor.com/"
+    //const val RESPONSE_END_URL = "b/60fa8fefa917050205ce5470"
+    const val RESPONSE_END_URL = "pizzas"
+    const val ROOM_DATABASE_NAME = "response_database"
+}
